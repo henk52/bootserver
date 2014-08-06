cd3771b82c1166d6d94ea06bfa17ac8a7c19deb5
Your Name <you@example.com>
2014-08-06 23:10:46 +0200

    Should be good to go.

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,143 @@
 #
 # Copyright 2014 Your name here, unless otherwise noted.
 #
-class bootserver {
+class bootserver (
+  $szDnsmasqProcessOwnerName = 'nobody'
+  $szWebProcessOwnerName = 'lighttpd'
+  # This information should probably go into hiera.
+  $szIpAddressForSupportingKickStart
+  # This is the C class subnet address, used by the dnsmasq.conf template
+  $szClassCSubnetAddress
+  $szTftpBaseDirectory = '/var/tftp'
+  $szKickStartBaseDirectory = '/var/ks'
+)
+{
+  # Create a kickstart server that will host files over NFS.
+
+  # This is the directory where the kick start configuration files are stored.
+  # This is exported via NFS.
+  $szKickStartConfsDir      = "$szKickStartBaseDirectory/configs"
+
+  # This is where ??? is stored?
+  $szKickStartImageDir      = "$szKickStartBaseDirectory/images"
+
+  # TODO C Use this both for post pkg install as well as kickstart installs.
+  $szKickStartRepoExtrasDir = "$szKickStartBaseDirectory/extrarepos"
+
+  $szKickStartPuppetExtrasDir = "$szKickStartBaseDirectory/puppetextras"
+
+  $szDnsMasqBaseDirectory = "/etc/dnsmasq.d"
+
+  $szDnsMasqStaticDirectory = "$szDnsMasqBaseDirectory/static"
+
+  $szDnsMasqHostConfigurationName = "hosts.conf"
+
+file { $szKickStartBaseDirectory:
+  ensure => directory,
+}
+
+file { $szKickStartConfsDir:
+  ensure  => directory,
+  require => File [ $szKickStartBaseDirectory ],
+}
+
+file { $szKickStartImageDir:
+  ensure  => directory,
+  require => File [ $szKickStartBaseDirectory ],
+}
+
+
+# ===== DNSMASQ
+package { 'dnsmasq':
+  ensure => present,
+}
+
+service { 'dnsmasq':
+  ensure  => running,
+  require => Package [ 'dnsmasq'],
+}
+
+
+# http://blogging.dragon.org.uk/index.php/mini-howtos/howto-setup-dnsmasq-on-fedora
+
+# dnsmasq.conf
+file { $szDnsMasqBaseDirectory:
+  ensure  => directory,
+  require => [ Package [ 'dnsmasq' ], File [ $szTftpBaseDirectory ], ],
+}
+
+
+file { $szDnsMasqStaticDirectory:
+  ensure  => directory,
+  require => File [ $szDnsMasqBaseDirectory ],
+}
+
+file { '/etc/dnsmasq.conf':
+  ensure  => present,
+  content => template('/vagrant/templates/dnsmasq_conf.erb'),
+  require => File [ $szDnsMasqStaticDirectory ],
+  notify  => Service [ 'dnsmasq' ]
+}
+
+file { "$szDnsMasqStaticDirectory/$szDnsMasqHostConfigurationName":
+  ensure  => present,
+  require => File [ $szDnsMasqStaticDirectory ],
+}
+
+file { $szTftpBaseDirectory:
+  ensure  => directory,
+  owner   => $szDnsmasqProcessOwnerName,
+}
+
+# TODO put the freaking SELinux into permisive mode to allow dnsmaq to run.
+
+
+package { 'syslinux':
+  ensure => present,
+}
+
+file { "$szTftpBaseDirectory/pxelinux.0":
+  ensure  => file,
+  source  => '/usr/share/syslinux/pxelinux.0',
+  owner   => $szDnsmasqProcessOwnerName,
+  mode    => 444,
+  require => Package[ 'syslinux' ],
+}
+
+file { "$szTftpBaseDirectory/pxelinux.cfg":
+  ensure  => directory,
+  owner   => $szDnsmasqProcessOwnerName,
+  mode    => 444,
+  require => File [ $szTftpBaseDirectory ],
+}
+
+file { "$szTftpBaseDirectory/pxelinux.cfg/default":
+  ensure  => file,
+  source  => '/vagrant/files/pxe_cfg_default',
+  owner   => $szDnsmasqProcessOwnerName,
+  mode    => 444,
+  require => File [ "$szTftpBaseDirectory/pxelinux.cfg" ],
+}
+
+# ------------------  Create the host repo thing.
+# This for creating the repo for the packages that are not in the images (the copy of the ISOs).
+# See: http://www.techrepublic.com/blog/linux-and-open-source/create-your-own-yum-repository/609/
+package { 'createrepo':
+  ensure => present,
+}
+
+file { "$szKickStartRepoExtrasDir":
+  ensure  => directory,
+  owner   => $szWebProcessOwnerName,
+  require => File [ "$szKickStartBaseDirectory" ],
+}
+
+
+file { "$szKickStartPuppetExtrasDir":
+  ensure => directory,
+  owner   => $szWebProcessOwnerName,
+  require => File [ "$szKickStartBaseDirectory" ],
+}
 
 
 }
